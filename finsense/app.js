26436927273541e5db326817eb1c064e8622cf4d
Parisa Kianmajd <parisa@Parisas-MacBook-Pro.local>
2016-07-09 16:48:26 -0700

    added instagram oauth

--- finsense/app.js
+++ finsense/app.js
@@ -4,11 +4,43 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var api = require('instagram-node').instagram();
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+//add client_id and client_secret here!
+api.use({ client_id: client_id,
+         client_secret: client_secret });
+
+var redirect_uri = 'http://localhost:3000/handleauth';
+ 
+exports.authorize_user = function(req, res) {
+  res.redirect(api.get_authorization_url(redirect_uri, { }));
+};
+var fs = require('fs'); 
+exports.handleauth = function(req, res) {
+  api.authorize_user(req.query.code, redirect_uri, function(err, result) {
+    if (err) {
+      console.log(err.body);
+      res.send("Didn't work");
+    } else {
+      console.log('Yay! Access token is ' + result.access_token);
+      fs.writeFile('access.txt', result.access_token, function (err) {
+     	 if (err) return console.log(err);
+ 	 console.log(result.access_token + ' > access.txt');
+      });
+      res.send('You made it!!');
+    }
+  });
+};
+
+
+// This is where you would initially send users to authorize 
+app.get('/authorize_user', exports.authorize_user);
+// This is your redirect URI 
+app.get('/handleauth', exports.handleauth);
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
