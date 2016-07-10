76d4432ce2c744495e092fb6dddef62613172fcb
Raymond Sapida <ray.sapida@gmail.com>
2016-07-10 14:21:04 -0700

    Hide logout button if user is not signed in

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  if (req.session.token !== undefined) {
+    var user = {
+      username: req.session.username,
+      profilePicture: req.session.profil_picture,
+      id: req.session.id
+    };
+  }
+  res.render('index', { title: 'Express', user: user });
 });
 
 module.exports = router;
